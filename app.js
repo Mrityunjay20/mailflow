f18ee9dc3c96f4d465e6bb434001d830dc453370
MJX.SH <70670860+Mrityunjay20@users.noreply.github.com>
2023-12-30 22:35:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const cors = require('cors');
 
 
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://mailflowai-ten.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(express.json());
 app.set('trust-proxy',true);
 app.use(bodyParser.urlencoded({extended:true}));
