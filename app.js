e252cd7f94087341e7166639f8a6f9ee56dd9aab
MJX.SH <70670860+Mrityunjay20@users.noreply.github.com>
2023-12-30 22:42:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
 const mongoose = require('mongoose');
-const passport = require('passport')
-const authRouters = require('./routes/authRoute')
+const passport = require('passport');
+const authRouters = require('./routes/authRoute');
 const profileRoutes = require('./routes/profile');
 const cors = require('cors');
 
-
+// Combine CORS and JSON parsing middleware
 app.use(cors());
+app.use(express.json());
+
+// CORS headers
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://mailflowai-ten.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -18,31 +21,30 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.json());
-app.set('trust-proxy',true);
-app.use(bodyParser.urlencoded({extended:true}));
+// Other middleware
+app.set('trust-proxy', true);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+require('./middleware/passport');
+
+// Routes
 app.use(authRouters);
 app.use(profileRoutes);
 
-app.use(passport.initialize());
-require('./middleware/passport')
-
-
+// MongoDB connection
 const dbName = 'MailFlow';
 const url = process.env.MONGO_URL;
-mongoose.connect(url,{ dbName, useNewUrlParser: true, useUnifiedTopology: true }).then(result=>{
-    app.listen(4000)
-}).catch(err=>{
-    console.log(err);
+mongoose.connect(url, { dbName, useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        app.listen(4000, () => {
+            console.log('Server is running on port 4000');
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+// 404 handler
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
-// app.get('/tries',passport.authenticate('jwt',{session:false}), (req,res)=>{
-//     console.log("tries");
-//     res.send(req.body);
-// });
-
-
-
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-})
