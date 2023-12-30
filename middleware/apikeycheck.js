4a3d5d6c51d5d2d66675a28a1ec0bf79757dd27d
MJX.SH <70670860+Mrityunjay20@users.noreply.github.com>
2023-12-30 22:32:24 +0530

    Update apikeycheck.js

--- middleware/apikeycheck.js
+++ middleware/apikeycheck.js
@@ -17,15 +17,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const apiKeyMiddleware = (req, res, next) => {
   
-  const apiKey = req.headers['x-api-key'];
+  // const apiKey = req.headers['x-api-key'];
 
-  console.log(apiKey);
-  // Replace 'YOUR_SECRET_API_KEY' with your actual API key
-  const secretApiKey = '54321a';
+  // console.log(apiKey);
+  // // Replace 'YOUR_SECRET_API_KEY' with your actual API key
+  // const secretApiKey = '54321a';
 
-  if (!apiKey || apiKey !== secretApiKey) {
-    return res.status(405).json({ error: 'Invalid API key.' });
-  }
+  // if (!apiKey || apiKey !== secretApiKey) {
+  //   return res.status(405).json({ error: 'Invalid API key.' });
+  // }
 
   // If the API key is valid, proceed to the next middleware/route handler
   next();
